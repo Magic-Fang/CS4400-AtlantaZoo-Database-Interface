44f6dd5045b2604cf1993a121daa292b97db1e7c
Zhou <34240816+Magic-Fang@users.noreply.github.com>
2018-12-20 16:37:55 -0500

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,5 @@
+# To run the program correctly, change the lines below with your database server or local database.
+host_name = 'academic-mysql.cc.gatech.edu'
+database_name = 'cs4400_group1' 
+user_name = 'cs4400_group1'
+password = 'ehiHGsY7'
